b111ccbc33836323333b8f0f339c27d766f40925
shun8800 <51105976+shun8800@users.noreply.github.com>
2021-06-30 09:03:46 +0900

    1. 회원가입 시 아이디 기본 5글자 이상 설정 2. 아이디 맨 앞에 direc이 붙으면 감독권한으로 로그인(한화, 넥센은 만들어둔 상태) 3. 라인업 등록 시에 아이디로 접근하여 해당 팀 버튼과 팀 선수 스피너 표시 ex)direchan으로 로그인시 [한화] 버튼과 [한화 선수] 스피너 표시 4. 감독 권한으로 로그인하여 라인업 등록 완료하면 기록 권한으로 이동

---
+++ app/src/main/java/com/example/nowinning/StartManager.java
@@ -0,0 +1,69 @@
+package com.example.nowinning;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartManager extends AppCompatActivity {
+    Button lineup_button, record_button, community_button, vs_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager);
+
+        lineup_button = (Button) findViewById(R.id.lineup_button);
+        record_button = (Button) findViewById(R.id.record_button);
+        community_button = (Button) findViewById(R.id.community_button);
+        vs_button = (Button) findViewById(R.id.vs_button);
+
+        lineup_button.setOnClickListener(new View.OnClickListener() { // 라인업 등록
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(StartManager.this, StartList.class);
+                startActivity(intent);
+
+            }
+        });
+
+        record_button.setOnClickListener(new View.OnClickListener() { // 선수 기록
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(StartManager.this,"선수 기록을 확인할 수 있는 버튼입니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(StartManager.this, StartRecorder.class);
+                startActivity(intent);
+
+            }
+        });
+
+        community_button.setOnClickListener(new View.OnClickListener() { // 커뮤니티
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(StartManager.this,"커뮤니티에 접근할 수 있는 버튼입니다.", Toast.LENGTH_SHORT).show();
+                /*Intent intent = new Intent(StartRecorder.this, 여기에 클래스 입력.class);
+                startActivity(intent);*/
+
+            }
+        });
+
+        vs_button.setOnClickListener(new View.OnClickListener() { // 상대 전적
+            @Override
+            public void onClick(View v) {
+
+                //Toast.makeText(StartRecorder.this,"선택한 선수의 역대 전적을 확인할 수 있는 버튼입니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(StartManager.this, StartVs.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
+
